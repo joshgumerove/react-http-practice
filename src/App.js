b69f66715edfb0d2ef3e36fbcaadf4db7b91ccc9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-10-06 16:26:19 -0400

    useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,12 +8,12 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
-        throw new Error("the response is bad");
+        throw new Error("Bad response");
       }
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -29,7 +29,11 @@ function App() {
       setError(err.message);
     }
     setIsLoading(false);
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
   if (movies.length > 0) {
